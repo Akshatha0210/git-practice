25827fc6fd4a5bb7e1c672b2c0697ee9ac8bf116
Akshatha0210 <akshatha.prashanth@withum.com>
2026-07-24 16:08:11 +0530

    Increase discount to 15 percent

--- Discount.java
+++ Discount.java
@@ -5,10 +5,10 @@ public class Discount {
     public static void main(String[] args) {
 
         Function<Double, Double> discount =
-                price -> price * 0.10;
+                price -> price * 0.15;
 
         double result = discount.apply(5000.0);
 
-        System.out.println(result);
+       System.out.println("FeatureA Discount = " + result);
     }
 }
